32497ed1c6a6637886167a6f77d4f731692bc44d
Mattwhited <matthewwhited@hotmail.com>
2023-05-28 20:25:34 -0500

    Write thoughts out on the rockPaperScissors game.
    just trying to sparse my thoughts out.

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// My plan is to write out my thought process behind 
+// writing script for the rockPaperScissors game. 
+
+// i will need a prompt or an input asking the user to
+// select either rock paper or scissors. 
+
+// the computer will then need to spit out its own random choice. 
+
+// establish a winner and let them know who it is. 
